56b99abc986e779e332690cb74558214f939a30a
VincentGenesius <vincentgtkj@gmail.com>
2026-02-03 07:41:26 +0700

    feat: implement router app

--- public/index.php
+++ public/index.php
@@ -5,6 +5,11 @@ use App\Core\Router; //artinya menggunakan class "Router" yang telah dibuat
 
 $router = new Router(); //memanggil class Router
 
+// Register Routes
+$router->add('GET', '/students', 'StudentController', 'index');
+$router->add('GET', '/students/create', 'StudentController', 'create');
+$router->add('GET', '/students/{id}', 'StudentController', 'show');
+
 $router->run();
 
 ?>
