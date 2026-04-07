a4f2e26746f3fe03f5c03981aae290840003c561
VincentGenesius <vincentgtkj@gmail.com>
2026-04-07 07:28:06 +0700

    feat: implement submit button in student create page

--- app/views/students/create.php
+++ app/views/students/create.php
@@ -18,7 +18,7 @@
 
 
     <main class="grow container mx-auto">
-        <div class="mt-8 space-y-4">
+        <div class="mt-8 mb-6 space-y-4">
             <div class="bg-white shadow rounded-lg p-4">
                 <h1 class="text-2xl font-bold">Tambah Siswa</h1>
                 <p>Menambahkan Siswa ke dalam sistem</p>
@@ -31,17 +31,25 @@
                     <label class="block font-bold" for="name">Nama</label>
                     <input class="w-full border rounded-lg py-2 px-4" type="text" name="name" id="name" placeholder="Masukkan Nama">
                 </div>
+
                 <div class="space-y-2">
                     <label class="block font-bold" for="nis">NIS</label>
                     <input class="w-full border rounded-lg py-2 px-4" type="text" name="nis" id="nis" placeholder="Masukkan NIS">
                 </div>
+
                 <div class="space-y-2">
                     <label class="block font-bold" for="class">Kelas</label>
                     <input class="w-full border rounded-lg py-2 px-4" type="text" name="class" id="class" placeholder="Masukkan Kelas">
                 </div>
+
                 <div class="space-y-2">
                     <label class="block font-bold" for="phone_number">No Telepon</label>
                     <input class="w-full border rounded-lg py-2 px-4" type="text" name="phone_number" id="phone_number" placeholder="Masukkan No Telepon">
+                </div>
+
+                <div class="flex justify-end gap-4 col-span-2">
+                    <a href="/students" class="py-2 px-4 bg-gray-100 rounded-lg">Kembali</a>
+                    <button type="submit" class="py-2 px-4 bg-blue-500 rounded-lg text-white">Simpan</button>
                 </div>
             </form>
         </div>
