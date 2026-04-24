526f478ac0a85f142ff1fe3078008251dc07b6f0
VincentGenesius <vincentgtkj@gmail.com>
2026-04-24 11:34:19 +0700

    feat: implement edit and delete student

--- app/controllers/StudentController.php
+++ app/controllers/StudentController.php
@@ -37,7 +37,13 @@ class StudentController extends Controller
 
     public function edit(string $id)
     {
-        $this->view('students.edit');
+        $id = intval($id);
+        $studentModel = new Student();
+        $student = $studentModel->getStudentById($id);
+
+        $this->view('students.edit', [
+            'student' => $student
+        ]);
     }
 
     public function store()
@@ -46,4 +52,17 @@ class StudentController extends Controller
         $studentModel->insert($_POST);
     }
 
+    public function update(string $id)
+    {
+        $id = intval($id);
+        $studentModel = new Student();
+        $studentModel->update($_POST, $id);
+    }
+
+    public function destroy(string $id)
+    {
+        $id = intval($id);
+        $studentModel = new Student();
+        $studentModel->delete($id);
+    }
 }
